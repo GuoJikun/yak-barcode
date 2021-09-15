96a05a340a59c15a74d689e990b61768bcd6864b
GuoJikun <guojikun@y-open.com>
2021-09-15 16:34:42 +0800

    fix:修复在vuepress@next中无法使用的问题

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -12,13 +12,15 @@ export default defineConfig({
       name: "barcode",
       fileName: (format) => `barcode.${format}.js`,
     },
+    minify: "esbuild",
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ["vue"],
+      external: ["vue", "jsbarcode"],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: "Vue",
+          jsbarcode: "JsBarcode",
         },
         exports: "named",
       },
